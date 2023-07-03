5268953d3175af128392cab206faef767ef474c7
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-07-03 18:46:29 +0430

    fix a small bug in js file

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const container = $(".container");
-const circle1 = $(".color-1");
-const circle2 = $(".color-2");
+$( function () {
+    const container = $(".container");
+    const circle1 = $(".color-1");
+    const circle2 = $(".color-2");
 
-circle1.click(() => {
-    container.removeClass("change");
-})
+    circle1.click(() => {
+        container.removeClass("change");
+    })
 
-circle2.click(() => {
-    container.addClass("change");
+    circle2.click(() => {
+        container.addClass("change");
+    })
 })
