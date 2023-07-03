70bcbdde936d86aa104f7af5dfe7c4f8b412360b
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-07-03 18:40:53 +0430

    finish

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = $(".container");
+const circle1 = $(".color-1");
+const circle2 = $(".color-2");
+
+circle1.click(() => {
+    container.removeClass("change");
+})
+
+circle2.click(() => {
+    container.addClass("change");
+})
